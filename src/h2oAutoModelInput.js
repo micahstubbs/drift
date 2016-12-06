a773c324f7667b7deb6a3ec8793bdbcf0ebf0668
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-06 15:51:59 -0800

    abstract out h2oAutoModelInput

---
+++ src/h2oAutoModelInput.js
@@ -0,0 +1,111 @@
+export function h2oAutoModelInput(_, _go, opts) {
+  var lodash = window._;
+  var Flow = window.Flow;
+  var buildModel;
+  var defaultMaxRunTime;
+  var _canBuildModel;
+  var _column;
+  var _columns;
+  var _frame;
+  var _frames;
+  var _hasFrame;
+  var _maxRunTime;
+  if (opts == null) {
+    opts = {};
+  }
+  _frames = Flow.Dataflow.signal([]);
+  _frame = Flow.Dataflow.signal(null);
+  _hasFrame = Flow.Dataflow.lift(_frame, function (frame) {
+    if (frame) {
+      return true;
+    }
+    return false;
+  });
+  _columns = Flow.Dataflow.signal([]);
+  _column = Flow.Dataflow.signal(null);
+  _canBuildModel = Flow.Dataflow.lift(_frame, _column, function (frame, column) {
+    return frame && column;
+  });
+  defaultMaxRunTime = 3600;
+  _maxRunTime = Flow.Dataflow.signal(defaultMaxRunTime);
+  buildModel = function () {
+    var arg;
+    var maxRunTime;
+    var parsed;
+    maxRunTime = defaultMaxRunTime;
+    if (!lodash.isNaN(parsed = parseInt(_maxRunTime(), 10))) {
+      maxRunTime = parsed;
+    }
+    arg = {
+      frame: _frame(),
+      column: _column(),
+      maxRunTime
+    };
+    return _.insertAndExecuteCell('cs', `buildAutoModel ${JSON.stringify(arg)}`);
+  };
+  _.requestFrames(function (error, frames) {
+    var frame;
+    if (error) {
+      // empty
+    } else {
+      _frames(function () {
+        var _i;
+        var _len;
+        var _results;
+        _results = [];
+        for (_i = 0, _len = frames.length; _i < _len; _i++) {
+          frame = frames[_i];
+          if (!frame.is_text) {
+            _results.push(frame.frame_id.name);
+          }
+        }
+        return _results;
+      }());
+      if (opts.frame) {
+        _frame(opts.frame);
+      }
+    }
+  });
+  Flow.Dataflow.react(_frame, function (frame) {
+    if (frame) {
+      return _.requestFrameSummaryWithoutData(frame, function (error, frame) {
+        var column;
+        if (error) {
+          // empty
+        } else {
+          _columns(function () {
+            var _i;
+            var _len;
+            var _ref;
+            var _results;
+            _ref = frame.columns;
+            _results = [];
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              column = _ref[_i];
+              _results.push(column.label);
+            }
+            return _results;
+          }());
+          if (opts.column) {
+            _column(opts.column);
+            return delete opts.column;
+          }
+        }
+      });
+    }
+    return _columns([]);
+  });
+  lodash.defer(_go);
+  return {
+    frames: _frames,
+    frame: _frame,
+    hasFrame: _hasFrame,
+    columns: _columns,
+    column: _column,
+    maxRunTime: _maxRunTime,
+    canBuildModel: _canBuildModel,
+    buildModel,
+    template: 'flow-automodel-input'
+  };
+};
+   
--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ import { h2oColumnSummaryOutput } from './h2oColumnSummaryOutput';
 import { h2oCloudOutput } from './h2oCloudOutput';
 import { h2oCancelJobOutput } from './h2oCancelJobOutput';
 import { h2oBindFramesOutput } from './h2oBindFramesOutput';
+import { h2oAutoModelInput } from './h2oAutoModelInput';
 
 (function () {
   var lodash = window._; window.Flow = {}; window.H2O = {}; (function () {
@@ -9122,7 +9123,7 @@ import { h2oBindFramesOutput } from './h2oBindFramesOutput';
           case buildModel:
             return _fork(proceed, H2O.ModelInput, args);
           case buildAutoModel:
-            return _fork(proceed, H2O.AutoModelInput, args);
+            return _fork(proceed, h2oAutoModelInput, args);
           case predict:
           case getPrediction:
             return _fork(proceed, h2oPredictInput, args);
@@ -9302,116 +9303,6 @@ import { h2oBindFramesOutput } from './h2oBindFramesOutput';
           return _results;
         }(),
         template: 'flow-assist'
-      };
-    };
-  }.call(this));
-  (function () {
-    H2O.AutoModelInput = function (_, _go, opts) {
-      var buildModel;
-      var defaultMaxRunTime;
-      var _canBuildModel;
-      var _column;
-      var _columns;
-      var _frame;
-      var _frames;
-      var _hasFrame;
-      var _maxRunTime;
-      if (opts == null) {
-        opts = {};
-      }
-      _frames = Flow.Dataflow.signal([]);
-      _frame = Flow.Dataflow.signal(null);
-      _hasFrame = Flow.Dataflow.lift(_frame, function (frame) {
-        if (frame) {
-          return true;
-        }
-        return false;
-      });
-      _columns = Flow.Dataflow.signal([]);
-      _column = Flow.Dataflow.signal(null);
-      _canBuildModel = Flow.Dataflow.lift(_frame, _column, function (frame, column) {
-        return frame && column;
-      });
-      defaultMaxRunTime = 3600;
-      _maxRunTime = Flow.Dataflow.signal(defaultMaxRunTime);
-      buildModel = function () {
-        var arg;
-        var maxRunTime;
-        var parsed;
-        maxRunTime = defaultMaxRunTime;
-        if (!lodash.isNaN(parsed = parseInt(_maxRunTime(), 10))) {
-          maxRunTime = parsed;
-        }
-        arg = {
-          frame: _frame(),
-          column: _column(),
-          maxRunTime
-        };
-        return _.insertAndExecuteCell('cs', `buildAutoModel ${JSON.stringify(arg)}`);
-      };
-      _.requestFrames(function (error, frames) {
-        var frame;
-        if (error) {
-          // empty
-        } else {
-          _frames(function () {
-            var _i;
-            var _len;
-            var _results;
-            _results = [];
-            for (_i = 0, _len = frames.length; _i < _len; _i++) {
-              frame = frames[_i];
-              if (!frame.is_text) {
-                _results.push(frame.frame_id.name);
-              }
-            }
-            return _results;
-          }());
-          if (opts.frame) {
-            _frame(opts.frame);
-          }
-        }
-      });
-      Flow.Dataflow.react(_frame, function (frame) {
-        if (frame) {
-          return _.requestFrameSummaryWithoutData(frame, function (error, frame) {
-            var column;
-            if (error) {
-              // empty
-            } else {
-              _columns(function () {
-                var _i;
-                var _len;
-                var _ref;
-                var _results;
-                _ref = frame.columns;
-                _results = [];
-                for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                  column = _ref[_i];
-                  _results.push(column.label);
-                }
-                return _results;
-              }());
-              if (opts.column) {
-                _column(opts.column);
-                return delete opts.column;
-              }
-            }
-          });
-        }
-        return _columns([]);
-      });
-      lodash.defer(_go);
-      return {
-        frames: _frames,
-        frame: _frame,
-        hasFrame: _hasFrame,
-        columns: _columns,
-        column: _column,
-        maxRunTime: _maxRunTime,
-        canBuildModel: _canBuildModel,
-        buildModel,
-        template: 'flow-automodel-input'
       };
     };
   }.call(this));
