23b73e607763836a273b26a19a2f0e58940ba80e
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-08 18:13:47 -0800

    update util for prefer const

--- src/util/util.js
+++ src/util/util.js
@@ -1,9 +1,7 @@
 export function util() {
   const Flow = window.Flow;
-  let getFileBaseName;
-  let validateFileExtension;
-  validateFileExtension = (filename, extension) => filename.indexOf(extension, filename.length - extension.length) !== -1;
-  getFileBaseName = (filename, extension) => Flow.Util.sanitizeName(filename.substr(0, filename.length - extension.length));
+  const validateFileExtension = (filename, extension) => filename.indexOf(extension, filename.length - extension.length) !== -1;
+  const getFileBaseName = (filename, extension) => Flow.Util.sanitizeName(filename.substr(0, filename.length - extension.length));
   H2O.Util = {
     validateFileExtension,
     getFileBaseName
