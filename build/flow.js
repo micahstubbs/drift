07e814c09d4ab2914a19e7cb76c5fe555c3ab698
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-08 22:55:02 -0800

    lint localStorage for prefer-const

--- build/flow.js
+++ build/flow.js
@@ -8188,28 +8188,20 @@
   function localStorage() {
     const lodash = window._;
     const Flow = window.Flow;
-    let keyOf;
-    let list;
-    let purge;
-    let purgeAll;
-    let read;
-    let write;
-    let _ls;
     if (!(typeof window !== 'undefined' && window !== null ? window.localStorage : void 0)) {
       return;
     }
-    _ls = window.localStorage;
-    keyOf = (type, id) => `${ type }:${ id }`;
-    list = type => {
+    const _ls = window.localStorage;
+    const keyOf = (type, id) => `${ type }:${ id }`;
+    const list = type => {
       let i;
       let id;
       let key;
-      let objs;
       let t;
       let _i;
       let _ref;
       let _ref1;
-      objs = [];
+      const objs = [];
       for (i = _i = 0, _ref = _ls.length; _ref >= 0 ? _i < _ref : _i > _ref; i = _ref >= 0 ? ++_i : --_i) {
         key = _ls.key(i);
         _ref1 = key.split(':'), t = _ref1[0], id = _ref1[1];
@@ -8219,31 +8211,29 @@
       }
       return objs;
     };
-    read = (type, id) => {
+    const read = (type, id) => {
       let raw;
       if (raw = _ls.getobj(keyOf(type, id))) {
         return JSON.parse(raw);
       }
       return null;
     };
-    write = (type, id, obj) => _ls.setItem(keyOf(type, id), JSON.stringify(obj));
-    purge = (type, id) => {
+    const write = (type, id, obj) => _ls.setItem(keyOf(type, id), JSON.stringify(obj));
+    const purge = (type, id) => {
       if (id) {
         return _ls.removeItem(keyOf(type, id));
       }
       return purgeAll(type);
     };
-    purgeAll = type => {
-      let allKeys;
+    const purgeAll = type => {
       let i;
       let key;
       let _i;
       let _len;
-      allKeys = (() => {
+      const allKeys = (() => {
         let _i;
         let _ref;
-        let _results;
-        _results = [];
+        const _results = [];
         for (i = _i = 0, _ref = _ls.length; _ref >= 0 ? _i < _ref : _i > _ref; i = _ref >= 0 ? ++_i : --_i) {
           _results.push(_ls.key(i));
         }
