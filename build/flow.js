b90e712883c27af2c1ed0787ca6d5b6073886206
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-09 13:50:48 -0800

    lint imputeInput for rules prefer-const and no-undef

--- build/flow.js
+++ build/flow.js
@@ -1266,15 +1266,12 @@
   function imputeInput() {
     const lodash = window._;
     const Flow = window.Flow;
-    let createOptions;
-    let _allCombineMethods;
-    let _allMethods;
-    createOptions = options => {
+    const H2O = window.H2O;
+    const createOptions = options => {
       let option;
       let _i;
       let _len;
-      let _results;
-      _results = [];
+      const _results = [];
       for (_i = 0, _len = options.length; _i < _len; _i++) {
         option = options[_i];
         _results.push({
@@ -1284,51 +1281,35 @@
       }
       return _results;
     };
-    _allMethods = createOptions(['Mean', 'Median', 'Mode']);
-    _allCombineMethods = createOptions(['Interpolate', 'Average', 'Low', 'High']);
+    const _allMethods = createOptions(['Mean', 'Median', 'Mode']);
+    const _allCombineMethods = createOptions(['Interpolate', 'Average', 'Low', 'High']);
     H2O.ImputeInput = (_, _go, opts) => {
-      let impute;
-      let _canGroupByColumns;
-      let _canImpute;
-      let _canUseCombineMethod;
-      let _column;
-      let _columns;
-      let _combineMethod;
-      let _combineMethods;
-      let _frame;
-      let _frames;
-      let _groupByColumns;
-      let _hasFrame;
-      let _method;
-      let _methods;
       if (opts == null) {
         opts = {};
       }
-      _frames = Flow.Dataflow.signal([]);
-      _frame = Flow.Dataflow.signal(null);
-      _hasFrame = Flow.Dataflow.lift(_frame, frame => {
+      const _frames = Flow.Dataflow.signal([]);
+      const _frame = Flow.Dataflow.signal(null);
+      const _hasFrame = Flow.Dataflow.lift(_frame, frame => {
         if (frame) {
           return true;
         }
         return false;
       });
-      _columns = Flow.Dataflow.signal([]);
-      _column = Flow.Dataflow.signal(null);
-      _methods = _allMethods;
-      _method = Flow.Dataflow.signal(_allMethods[0]);
-      _canUseCombineMethod = Flow.Dataflow.lift(_method, method => method.value === 'median');
-      _combineMethods = _allCombineMethods;
-      _combineMethod = Flow.Dataflow.signal(_allCombineMethods[0]);
-      _canGroupByColumns = Flow.Dataflow.lift(_method, method => method.value !== 'median');
-      _groupByColumns = Flow.Dataflow.signals([]);
-      _canImpute = Flow.Dataflow.lift(_frame, _column, (frame, column) => frame && column);
-      impute = () => {
-        let arg;
+      const _columns = Flow.Dataflow.signal([]);
+      const _column = Flow.Dataflow.signal(null);
+      const _methods = _allMethods;
+      const _method = Flow.Dataflow.signal(_allMethods[0]);
+      const _canUseCombineMethod = Flow.Dataflow.lift(_method, method => method.value === 'median');
+      const _combineMethods = _allCombineMethods;
+      const _combineMethod = Flow.Dataflow.signal(_allCombineMethods[0]);
+      const _canGroupByColumns = Flow.Dataflow.lift(_method, method => method.value !== 'median');
+      const _groupByColumns = Flow.Dataflow.signals([]);
+      const _canImpute = Flow.Dataflow.lift(_frame, _column, (frame, column) => frame && column);
+      const impute = () => {
         let combineMethod;
         let groupByColumns;
-        let method;
-        method = _method();
-        arg = {
+        const method = _method();
+        const arg = {
           frame: _frame(),
           column: _column(),
           method: method.value
@@ -1353,8 +1334,7 @@
           _frames((() => {
             let _i;
             let _len;
-            let _results;
-            _results = [];
+            const _results = [];
             for (_i = 0, _len = frames.length; _i < _len; _i++) {
               frame = frames[_i];
               if (!frame.is_text) {
@@ -1378,10 +1358,8 @@
               _columns((() => {
                 let _i;
                 let _len;
-                let _ref;
-                let _results;
-                _ref = frame.columns;
-                _results = [];
+                const _ref = frame.columns;
+                const _results = [];
                 for (_i = 0, _len = _ref.length; _i < _len; _i++) {
                   column = _ref[_i];
                   _results.push(column.label);
