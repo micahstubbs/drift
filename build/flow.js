a05b9ba675fe80b93322b460f6cdb521eac9fae4
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-12 14:39:51 -0800

    lint flowApplication for rule prefer-const

--- build/flow.js
+++ build/flow.js
@@ -12015,16 +12015,13 @@
 
   function flowApplication(_, routines) {
     const Flow = window.Flow;
-    let _notebook;
-    let _renderers;
-    let _sandbox;
     flowApplicationContext(_);
-    _sandbox = flowSandbox(_, routines(_));
-    _renderers = Flow.Renderers(_, _sandbox);
+    const _sandbox = flowSandbox(_, routines(_));
+    const _renderers = Flow.Renderers(_, _sandbox);
     flowAnalytics(_);
     flowGrowl(_);
     flowAutosave(_);
-    _notebook = Flow.Notebook(_, _renderers);
+    const _notebook = Flow.Notebook(_, _renderers);
     return {
       context: _,
       sandbox: _sandbox,
--- src/flowApplication.js
+++ src/flowApplication.js
@@ -6,16 +6,13 @@ import { flowAutosave } from './flowAutosave';
 
 export function flowApplication(_, routines) {
   const Flow = window.Flow;
-  let _notebook;
-  let _renderers;
-  let _sandbox;
   flowApplicationContext(_);
-  _sandbox = flowSandbox(_, routines(_));
-  _renderers = Flow.Renderers(_, _sandbox);
+  const _sandbox = flowSandbox(_, routines(_));
+  const _renderers = Flow.Renderers(_, _sandbox);
   flowAnalytics(_);
   flowGrowl(_);
   flowAutosave(_);
-  _notebook = Flow.Notebook(_, _renderers);
+  const _notebook = Flow.Notebook(_, _renderers);
   return {
     context: _,
     sandbox: _sandbox,
