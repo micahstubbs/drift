21e2d66240795efab0657b1ce22657d544e35a9e
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-11 22:22:19 -0800

    lint h2oImportFilesOutput for rule prefer-const

--- src/h2oImportFilesOutput.js
+++ src/h2oImportFilesOutput.js
@@ -4,23 +4,16 @@ const flowPrelude = flowPreludeFunction();
 export function h2oImportFilesOutput(_, _go, _importResults) {
   const lodash = window._;
   const Flow = window.Flow;
-  let createImportView;
-  let parse;
-  let _allFrames;
-  let _canParse;
-  let _importViews;
-  let _title;
-  _allFrames = lodash.flatten(lodash.compact(lodash.map(_importResults, result => result.destination_frames)));
-  _canParse = _allFrames.length > 0;
-  _title = `${_allFrames.length} / ${_importResults.length} files imported.`;
-  createImportView = result => ({
+  const _allFrames = lodash.flatten(lodash.compact(lodash.map(_importResults, result => result.destination_frames)));
+  const _canParse = _allFrames.length > 0;
+  const _title = `${_allFrames.length} / ${_importResults.length} files imported.`;
+  const createImportView = result => ({
     files: result.files,
     template: 'flow-import-file-output'
   });
-  _importViews = lodash.map(_importResults, createImportView);
-  parse = () => {
-    let paths;
-    paths = lodash.map(_allFrames, flowPrelude.stringify);
+  const _importViews = lodash.map(_importResults, createImportView);
+  const parse = () => {
+    const paths = lodash.map(_allFrames, flowPrelude.stringify);
     return _.insertAndExecuteCell('cs', `setupParse source_frames: [ ${paths.join(',')} ]`);
   };
   lodash.defer(_go);
