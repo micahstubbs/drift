5ea55d7404d49f3fc126575142cfb43c49a8c13e
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-11 17:45:00 -0800

    lint h2oScalaIntpOutput for rule prefer-const

--- build/flow.js
+++ build/flow.js
@@ -3953,10 +3953,8 @@
   function h2oScalaIntpOutput(_, _go, _result) {
     const lodash = window._;
     const Flow = window.Flow;
-    let createScalaIntpView;
-    let _scalaIntpView;
-    _scalaIntpView = Flow.Dataflow.signal(null);
-    createScalaIntpView = result => ({
+    const _scalaIntpView = Flow.Dataflow.signal(null);
+    const createScalaIntpView = result => ({
       session_id: result.session_id
     });
     _scalaIntpView(createScalaIntpView(_result));
