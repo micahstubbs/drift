7cd3d573585b91dd6872892c9d78bbf3b6d8bb42
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-11 22:04:58 -0800

    lint h2oImportModelInput for rule prefer-const

--- build/flow.js
+++ build/flow.js
@@ -4778,17 +4778,13 @@
   function h2oImportModelInput(_, _go, path, opt) {
     const lodash = window._;
     const Flow = window.Flow;
-    let importModel;
-    let _canImportModel;
-    let _overwrite;
-    let _path;
     if (opt == null) {
       opt = {};
     }
-    _path = Flow.Dataflow.signal(path);
-    _overwrite = Flow.Dataflow.signal(opt.overwrite);
-    _canImportModel = Flow.Dataflow.lift(_path, path => path && path.length);
-    importModel = () => _.insertAndExecuteCell('cs', `importModel ${ flowPrelude$31.stringify(_path()) }, overwrite: ${ _overwrite() ? 'true' : 'false' }`);
+    const _path = Flow.Dataflow.signal(path);
+    const _overwrite = Flow.Dataflow.signal(opt.overwrite);
+    const _canImportModel = Flow.Dataflow.lift(_path, path => path && path.length);
+    const importModel = () => _.insertAndExecuteCell('cs', `importModel ${ flowPrelude$31.stringify(_path()) }, overwrite: ${ _overwrite() ? 'true' : 'false' }`);
     lodash.defer(_go);
     return {
       path: _path,
