51a9901fb3a5879a4dba62516c657c5a4c068e8c
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-11 22:31:04 -0800

    lint h2oH2OFrameOutput for rule prefer-const

--- build/flow.js
+++ build/flow.js
@@ -3445,10 +3445,8 @@
   function h2oH2OFrameOutput(_, _go, _result) {
     const lodash = window._;
     const Flow = window.Flow;
-    let createH2oFrameView;
-    let _h2oframeView;
-    _h2oframeView = Flow.Dataflow.signal(null);
-    createH2oFrameView = result => ({
+    const _h2oframeView = Flow.Dataflow.signal(null);
+    const createH2oFrameView = result => ({
       h2oframe_id: result.h2oframe_id
     });
     _h2oframeView(createH2oFrameView(_result));
