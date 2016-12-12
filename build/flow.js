e363cbb5d443719e92308bcdcafb081cad923082
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-12 12:24:16 -0800

    lint flowFileUploadDialog for rule prefer-const

--- build/flow.js
+++ build/flow.js
@@ -10191,24 +10191,19 @@
 
   function flowFileUploadDialog(_, _go) {
     const Flow = window.Flow;
-    let accept;
-    let decline;
-    let uploadFile;
-    let _file;
-    let _form;
-    _form = Flow.Dataflow.signal(null);
-    _file = Flow.Dataflow.signal(null);
-    uploadFile = key => _.requestUploadFile(key, new FormData(_form()), (error, result) => _go({
+    const _form = Flow.Dataflow.signal(null);
+    const _file = Flow.Dataflow.signal(null);
+    const uploadFile = key => _.requestUploadFile(key, new FormData(_form()), (error, result) => _go({
       error,
       result
     }));
-    accept = () => {
+    const accept = () => {
       let file;
       if (file = _file()) {
         return uploadFile(file.name);
       }
     };
-    decline = () => _go(null);
+    const decline = () => _go(null);
     return {
       form: _form,
       file: _file,
