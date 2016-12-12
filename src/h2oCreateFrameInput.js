825507efe728af77afaf684d41f8a3f083583a3a
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-12 11:20:46 -0800

    lint h2oCreateFrameInput for rule prefer-const

--- src/h2oCreateFrameInput.js
+++ src/h2oCreateFrameInput.js
@@ -4,48 +4,27 @@ const flowPrelude = flowPreludeFunction();
 export function h2oCreateFrameInput(_, _go) {
   const lodash = window._;
   const Flow = window.Flow;
-  let createFrame;
-  let _binaryFraction;
-  let _binaryOnesFraction;
-  let _categoricalFraction;
-  let _columns;
-  let _factors;
-  let _hasResponse;
-  let _integerFraction;
-  let _integerRange;
-  let _key;
-  let _missingFraction;
-  let _randomize;
-  let _realRange;
-  let _responseFactors;
-  let _rows;
-  let _seed;
-  let _seed_for_column_types;
-  let _stringFraction;
-  let _timeFraction;
-  let _value;
-  _key = Flow.Dataflow.signal('');
-  _rows = Flow.Dataflow.signal(10000);
-  _columns = Flow.Dataflow.signal(100);
-  _seed = Flow.Dataflow.signal(7595850248774472000);
-  _seed_for_column_types = Flow.Dataflow.signal(-1);
-  _randomize = Flow.Dataflow.signal(true);
-  _value = Flow.Dataflow.signal(0);
-  _realRange = Flow.Dataflow.signal(100);
-  _categoricalFraction = Flow.Dataflow.signal(0.1);
-  _factors = Flow.Dataflow.signal(5);
-  _integerFraction = Flow.Dataflow.signal(0.5);
-  _binaryFraction = Flow.Dataflow.signal(0.1);
-  _binaryOnesFraction = Flow.Dataflow.signal(0.02);
-  _timeFraction = Flow.Dataflow.signal(0);
-  _stringFraction = Flow.Dataflow.signal(0);
-  _integerRange = Flow.Dataflow.signal(1);
-  _missingFraction = Flow.Dataflow.signal(0.01);
-  _responseFactors = Flow.Dataflow.signal(2);
-  _hasResponse = Flow.Dataflow.signal(false);
-  createFrame = () => {
-    let opts;
-    opts = {
+  const _key = Flow.Dataflow.signal('');
+  const _rows = Flow.Dataflow.signal(10000);
+  const _columns = Flow.Dataflow.signal(100);
+  const _seed = Flow.Dataflow.signal(7595850248774472000);
+  const _seed_for_column_types = Flow.Dataflow.signal(-1);
+  const _randomize = Flow.Dataflow.signal(true);
+  const _value = Flow.Dataflow.signal(0);
+  const _realRange = Flow.Dataflow.signal(100);
+  const _categoricalFraction = Flow.Dataflow.signal(0.1);
+  const _factors = Flow.Dataflow.signal(5);
+  const _integerFraction = Flow.Dataflow.signal(0.5);
+  const _binaryFraction = Flow.Dataflow.signal(0.1);
+  const _binaryOnesFraction = Flow.Dataflow.signal(0.02);
+  const _timeFraction = Flow.Dataflow.signal(0);
+  const _stringFraction = Flow.Dataflow.signal(0);
+  const _integerRange = Flow.Dataflow.signal(1);
+  const _missingFraction = Flow.Dataflow.signal(0.01);
+  const _responseFactors = Flow.Dataflow.signal(2);
+  const _hasResponse = Flow.Dataflow.signal(false);
+  const createFrame = () => {
+    const opts = {
       dest: _key(),
       rows: _rows(),
       cols: _columns(),
