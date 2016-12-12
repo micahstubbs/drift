9609523c1f6da530ac20e5f6fc08d159e29f6008
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-12 12:11:00 -0800

    lint format for rules prefer-const and no-undef

--- src/format/format.js
+++ src/format/format.js
@@ -1,40 +1,33 @@
 export function format() {
   const lodash = window._;
   const Flow = window.Flow;
-  let Digits;
-  let formatDate;
-  let formatReal;
+  const d3 = window.d3;
   let formatTime;
-  let significantDigitsBeforeDecimal;
-  let __formatReal;
-  significantDigitsBeforeDecimal = value => 1 + Math.floor(Math.log(Math.abs(value)) / Math.LN10);
-  Digits = (digits, value) => {
-    let magnitude;
-    let sd;
+  const significantDigitsBeforeDecimal = value => 1 + Math.floor(Math.log(Math.abs(value)) / Math.LN10);
+  const Digits = (digits, value) => {
     if (value === 0) {
       return 0;
     }
-    sd = significantDigitsBeforeDecimal(value);
+    const sd = significantDigitsBeforeDecimal(value);
     if (sd >= digits) {
       return value.toFixed(0);
     }
-    magnitude = Math.pow(10, digits - sd);
+    const magnitude = Math.pow(10, digits - sd);
     return Math.round(value * magnitude) / magnitude;
   };
   if (typeof exports === 'undefined' || exports === null) {
     formatTime = d3.time.format('%Y-%m-%d %H:%M:%S');
   }
-  formatDate = time => {
+  const formatDate = time => {
     if (time) {
       return formatTime(new Date(time));
     }
     return '-';
   };
-  __formatReal = {};
-  formatReal = precision => {
+  const __formatReal = {};
+  const formatReal = precision => {
     let cached;
-    let format;
-    format = (cached = __formatReal[precision]) ? cached : __formatReal[precision] = precision === -1 ? lodash.identity : d3.format(`.${precision}f`);
+    const format = (cached = __formatReal[precision]) ? cached : __formatReal[precision] = precision === -1 ? lodash.identity : d3.format(`.${precision}f`);
     return value => format(value);
   };
   Flow.Format = {
