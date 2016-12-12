b577e113185da3c176ac0567d6a887666316437d
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-12 14:38:04 -0800

    "lint flowAutosave for rule prefer-const"

--- build/flow.js
+++ build/flow.js
@@ -11998,19 +11998,15 @@
 
   function flowAutosave(_) {
     const Flow = window.Flow;
-    let setDirty;
-    let setPristine;
-    let warnOnExit;
-    warnOnExit = e => {
-      let message;
-      message = 'Warning: you are about to exit Flow.';
+    const warnOnExit = e => {
+      const message = 'Warning: you are about to exit Flow.';
       if (e = e != null ? e : window.event) {
         e.returnValue = message;
       }
       return message;
     };
-    setDirty = () => window.onbeforeunload = warnOnExit;
-    setPristine = () => window.onbeforeunload = null;
+    const setDirty = () => window.onbeforeunload = warnOnExit;
+    const setPristine = () => window.onbeforeunload = null;
     return Flow.Dataflow.link(_.ready, () => {
       Flow.Dataflow.link(_.setDirty, setDirty);
       return Flow.Dataflow.link(_.setPristine, setPristine);
