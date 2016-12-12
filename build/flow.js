424673f740ef2bba75fe7e15cd0a0ad466d803d7
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-11 23:08:56 -0800

    lint h2oExportFrameInput for rule prefer-const

--- build/flow.js
+++ build/flow.js
@@ -4601,18 +4601,12 @@
   function h2oExportFrameInput(_, _go, frameKey, path, opt) {
     const lodash = window._;
     const Flow = window.Flow;
-    let exportFrame;
-    let _canExportFrame;
-    let _frames;
-    let _overwrite;
-    let _path;
-    let _selectedFrame;
-    _frames = Flow.Dataflow.signal([]);
-    _selectedFrame = Flow.Dataflow.signal(frameKey);
-    _path = Flow.Dataflow.signal(null);
-    _overwrite = Flow.Dataflow.signal(true);
-    _canExportFrame = Flow.Dataflow.lift(_selectedFrame, _path, (frame, path) => frame && path);
-    exportFrame = () => _.insertAndExecuteCell('cs', `exportFrame ${ flowPrelude$30.stringify(_selectedFrame()) }, ${ flowPrelude$30.stringify(_path()) }, overwrite: ${ _overwrite() ? 'true' : 'false' }`);
+    const _frames = Flow.Dataflow.signal([]);
+    const _selectedFrame = Flow.Dataflow.signal(frameKey);
+    const _path = Flow.Dataflow.signal(null);
+    const _overwrite = Flow.Dataflow.signal(true);
+    const _canExportFrame = Flow.Dataflow.lift(_selectedFrame, _path, (frame, path) => frame && path);
+    const exportFrame = () => _.insertAndExecuteCell('cs', `exportFrame ${ flowPrelude$30.stringify(_selectedFrame()) }, ${ flowPrelude$30.stringify(_path()) }, overwrite: ${ _overwrite() ? 'true' : 'false' }`);
     _.requestFrames((error, frames) => {
       let frame;
       if (error) {
@@ -4621,8 +4615,7 @@
         _frames((() => {
           let _i;
           let _len;
-          let _results;
-          _results = [];
+          const _results = [];
           for (_i = 0, _len = frames.length; _i < _len; _i++) {
             frame = frames[_i];
             _results.push(frame.frame_id.name);
--- src/h2oExportFrameInput.js
+++ src/h2oExportFrameInput.js
@@ -4,18 +4,12 @@ const flowPrelude = flowPreludeFunction();
 export function h2oExportFrameInput(_, _go, frameKey, path, opt) {
   const lodash = window._;
   const Flow = window.Flow;
-  let exportFrame;
-  let _canExportFrame;
-  let _frames;
-  let _overwrite;
-  let _path;
-  let _selectedFrame;
-  _frames = Flow.Dataflow.signal([]);
-  _selectedFrame = Flow.Dataflow.signal(frameKey);
-  _path = Flow.Dataflow.signal(null);
-  _overwrite = Flow.Dataflow.signal(true);
-  _canExportFrame = Flow.Dataflow.lift(_selectedFrame, _path, (frame, path) => frame && path);
-  exportFrame = () => _.insertAndExecuteCell('cs', `exportFrame ${flowPrelude.stringify(_selectedFrame())}, ${flowPrelude.stringify(_path())}, overwrite: ${(_overwrite() ? 'true' : 'false')}`);
+  const _frames = Flow.Dataflow.signal([]);
+  const _selectedFrame = Flow.Dataflow.signal(frameKey);
+  const _path = Flow.Dataflow.signal(null);
+  const _overwrite = Flow.Dataflow.signal(true);
+  const _canExportFrame = Flow.Dataflow.lift(_selectedFrame, _path, (frame, path) => frame && path);
+  const exportFrame = () => _.insertAndExecuteCell('cs', `exportFrame ${flowPrelude.stringify(_selectedFrame())}, ${flowPrelude.stringify(_path())}, overwrite: ${(_overwrite() ? 'true' : 'false')}`);
   _.requestFrames((error, frames) => {
     let frame;
     if (error) {
@@ -24,8 +18,7 @@ export function h2oExportFrameInput(_, _go, frameKey, path, opt) {
       _frames((() => {
         let _i;
         let _len;
-        let _results;
-        _results = [];
+        const _results = [];
         for (_i = 0, _len = frames.length; _i < _len; _i++) {
           frame = frames[_i];
           _results.push(frame.frame_id.name);
