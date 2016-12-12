3a289372e1695e23c88158ea6e800c4eae42dfca
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-12 12:25:33 -0800

    lint flowFileOpenDialog for rule prefer-const

--- build/flow.js
+++ build/flow.js
@@ -10139,29 +10139,21 @@
   function flowFileOpenDialog(_, _go) {
     const Flow = window.Flow;
     const H2O = window.H2O;
-    let accept;
-    let checkIfNameIsInUse;
-    let decline;
-    let uploadFile;
-    let _canAccept;
-    let _file;
-    let _form;
-    let _overwrite;
-    _overwrite = Flow.Dataflow.signal(false);
-    _form = Flow.Dataflow.signal(null);
-    _file = Flow.Dataflow.signal(null);
-    _canAccept = Flow.Dataflow.lift(_file, file => {
+    const _overwrite = Flow.Dataflow.signal(false);
+    const _form = Flow.Dataflow.signal(null);
+    const _file = Flow.Dataflow.signal(null);
+    const _canAccept = Flow.Dataflow.lift(_file, file => {
       if (file != null ? file.name : void 0) {
         return H2O.Util.validateFileExtension(file.name, '.flow');
       }
       return false;
     });
-    checkIfNameIsInUse = (name, go) => _.requestObjectExists('notebook', name, (error, exists) => go(exists));
-    uploadFile = basename => _.requestUploadObject('notebook', basename, new FormData(_form()), (error, filename) => _go({
+    const checkIfNameIsInUse = (name, go) => _.requestObjectExists('notebook', name, (error, exists) => go(exists));
+    const uploadFile = basename => _.requestUploadObject('notebook', basename, new FormData(_form()), (error, filename) => _go({
       error,
       filename
     }));
-    accept = () => {
+    const accept = () => {
       let basename;
       let file;
       if (file = _file()) {
@@ -10177,7 +10169,7 @@
         });
       }
     };
-    decline = () => _go(null);
+    const decline = () => _go(null);
     return {
       form: _form,
       file: _file,
