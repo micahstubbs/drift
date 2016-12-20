06a3db6f3a34d585b704d44a71ac707003c4cb96
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-19 17:52:16 -0800

    remove logging from proceed

--- build/flow.js
+++ build/flow.js
@@ -2325,10 +2325,6 @@
   }
 
   function proceed(_, func, args, go) {
-    console.log('proceed was called', proceed);
-    console.log('func', func);
-    console.log('args', args);
-    console.log('go', go);
     return go(null, render_(_, ...[{}, func].concat(args || [])));
   }
 
--- src/routines/proceed.js
+++ src/routines/proceed.js
@@ -1,10 +1,6 @@
 import { render_ } from './render_';
 
 export function proceed(_, func, args, go) {
-  console.log('proceed was called', proceed);
-  console.log('func', func);
-  console.log('args', args);
-  console.log('go', go);
   return go(null, render_(_,  ...[
     {},
     func
