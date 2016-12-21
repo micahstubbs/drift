70ad0038e057f4555966313774a93f4aed01e22b
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-21 15:20:18 -0800

    lint extendJob

--- src/routines/extendJob.js
+++ src/routines/extendJob.js
@@ -2,5 +2,5 @@ import { render_ } from './render_';
 
 export function extendJob(_, job) {
   const H2O = window.H2O;
-  return render_(_,  job, H2O.JobOutput, job);
+  return render_(_, job, H2O.JobOutput, job);
 }
