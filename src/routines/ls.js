e30cb6c457e81dba79d08aedc21aeefeec4080be
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-20 20:51:46 -0800

    add comments to ls about breaking model output and refactoring

--- src/routines/ls.js
+++ src/routines/ls.js
@@ -8,6 +8,9 @@ export function ls(obj) {
   if (_isFuture(obj)) {
     return _async(ls, obj);
   }
+  // if we refactor this for the rule no-cond-assign
+  // then the model output breaks
+  // TODO find a way to refactor that does not break model output
   if (inspectors = obj != null ? (_ref1 = obj._flow_) != null ? _ref1.inspect : void 0 : void 0) { // eslint-disable-line
     return lodash.keys(inspectors);
   }
