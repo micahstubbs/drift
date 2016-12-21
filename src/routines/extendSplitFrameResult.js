373939730bcc17dd3ea03b06d0584f1236c96194
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-21 15:08:15 -0800

    lint extendSplitFrameResult

--- src/routines/extendSplitFrameResult.js
+++ src/routines/extendSplitFrameResult.js
@@ -3,6 +3,6 @@ import { render_ } from './render_';
 import { h2oSplitFrameOutput } from '../h2oSplitFrameOutput';
 
 export function extendSplitFrameResult(_, result) {
-  render_(_,  result, h2oSplitFrameOutput, result);
+  render_(_, result, h2oSplitFrameOutput, result);
   return result;
-};
+}
