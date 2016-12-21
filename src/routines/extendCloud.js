6c34473b743c5808aacfb95be5f00d3bb33b5be7
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-21 13:57:55 -0800

    lint extendCloud

--- src/routines/extendCloud.js
+++ src/routines/extendCloud.js
@@ -3,5 +3,5 @@ import { render_ } from './render_';
 import { h2oCloudOutput } from '../h2oCloudOutput';
 
 export function extendCloud(_, cloud) {
-  return render_(_,  cloud, h2oCloudOutput, cloud);
+  return render_(_, cloud, h2oCloudOutput, cloud);
 }
