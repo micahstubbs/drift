9ca59f28f716ea8989268a4d9ec6d95b6c507e70
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-21 21:28:56 -0800

    abstract out computeSplits

--- build/flow.js
+++ build/flow.js
@@ -6479,47 +6479,6 @@
       //
       //
       //
-      computeSplits = (ratios, keys) => {
-        let i;
-        let key;
-        let part;
-        let parts;
-        let ratio;
-        let splits;
-        let sum;
-        let _i;
-        let _j;
-        let _len;
-        let _len1;
-        let _ref1;
-        parts = [];
-        sum = 0;
-        _ref1 = keys.slice(0, ratios.length);
-        for (i = _i = 0, _len = _ref1.length; _i < _len; i = ++_i) {
-          key = _ref1[i];
-          sum += ratio = ratios[i];
-          parts.push({
-            key,
-            ratio
-          });
-        }
-        parts.push({
-          key: keys[keys.length - 1],
-          ratio: 1 - sum
-        });
-        splits = [];
-        sum = 0;
-        for (_j = 0, _len1 = parts.length; _j < _len1; _j++) {
-          part = parts[_j];
-          splits.push({
-            min: sum,
-            max: sum + part.ratio,
-            key: part.key
-          });
-          sum += part.ratio;
-        }
-        return splits;
-      };
       requestBindFrames = (key, sourceKeys, go) => _.requestExec(`(assign ${ key } (cbind ${ sourceKeys.join(' ') }))`, (error, result) => {
         if (error) {
           return go(error);
