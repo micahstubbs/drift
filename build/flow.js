069e0e7fc599b1643c8385729a2e530003d08b7d
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-09 17:12:05 -0800

    abstract out findControl

--- build/flow.js
+++ build/flow.js
@@ -777,22 +777,6 @@
         _form.push(control);
       }
     }
-    const findControl = name => {
-      let controls;
-      let _l;
-      let _len3;
-      let _len4;
-      let _m;
-      for (_l = 0, _len3 = _controlGroups.length; _l < _len3; _l++) {
-        controls = _controlGroups[_l];
-        for (_m = 0, _len4 = controls.length; _m < _len4; _m++) {
-          control = controls[_m];
-          if (control.name === name) {
-            return control;
-          }
-        }
-      }
-    };
     const parameterTemplateOf = control => `flow-${ control.kind }-model-parameter`;
     const findFormField = name => lodash.find(_form, field => field.name === name);
     (() => {
