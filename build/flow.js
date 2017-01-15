1f5ec03fcdf183a59b42f5ab998baff95e367db8
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-15 01:44:57 -0800

    abstract out generateJavascript

--- build/flow.js
+++ build/flow.js
@@ -11290,21 +11290,24 @@
     };
   }
 
+  function generateJavascript(program, go) {
+    const Flow = window.Flow;
+    const escodegen = window.escodegen;
+    let error;
+    try {
+      return go(null, escodegen.generate(program));
+    } catch (_error) {
+      error = _error;
+      return go(new Flow.Error('Error generating javascript', error));
+    }
+  }
+
   function flowCoffeescriptKernel() {
     const lodash = window._;
     const Flow = window.Flow;
     const escodegen = window.escodegen;
     const esprima = window.esprima;
     const CoffeeScript = window.CoffeeScript;
-    const generateJavascript = (program, go) => {
-      let error;
-      try {
-        return go(null, escodegen.generate(program));
-      } catch (_error) {
-        error = _error;
-        return go(new Flow.Error('Error generating javascript', error));
-      }
-    };
     const compileJavascript = (js, go) => {
       let closure;
       let error;
--- src/flowCoffeescriptKernel/flowCoffeescriptKernel.js
+++ src/flowCoffeescriptKernel/flowCoffeescriptKernel.js
@@ -4,6 +4,7 @@ import { parseJavascript } from './parseJavascript';
 import { createRootScope } from './createRootScope';
 import { removeHoistedDeclarations } from './removeHoistedDeclarations';
 import { rewriteJavascript } from './rewriteJavascript';
+import { generateJavascript } from './generateJavascript';
 
 export function flowCoffeescriptKernel() {
   const lodash = window._;
@@ -11,15 +12,6 @@ export function flowCoffeescriptKernel() {
   const escodegen = window.escodegen;
   const esprima = window.esprima;
   const CoffeeScript = window.CoffeeScript;
-  const generateJavascript = (program, go) => {
-    let error;
-    try {
-      return go(null, escodegen.generate(program));
-    } catch (_error) {
-      error = _error;
-      return go(new Flow.Error('Error generating javascript', error));
-    }
-  };
   const compileJavascript = (js, go) => {
     let closure;
     let error;
