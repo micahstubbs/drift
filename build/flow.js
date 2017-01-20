1664e601260ada35a699519d76c888e2aab44490
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-20 12:36:39 -0800

    ensure that _ and function are passsed to _refresh inside of lodash delay. fix #106

--- build/flow.js
+++ build/flow.js
@@ -5088,12 +5088,13 @@
     }
 
     function _refresh(_, refresh) {
+      console.log('arguments passed to _refresh', arguments);
       const lodash = window._;
       refresh((error, model) => {
         if (!error) {
           _.output(createOutput(_));
           if (_.isLive()) {
-            return lodash.delay(_refresh, 2000);
+            return lodash.delay(_refresh.bind(this, _, refresh), 2000);
           }
         }
       });
--- src/h2oModelOutput/_refresh.js
+++ src/h2oModelOutput/_refresh.js
@@ -1,12 +1,13 @@
 import createOutput from './createOutput';
 
 export default function _refresh(_, refresh) {
+  console.log('arguments passed to _refresh', arguments);
   const lodash = window._;
   refresh((error, model) => {
     if (!error) {
       _.output(createOutput(_));
       if (_.isLive()) {
-        return lodash.delay(_refresh, 2000);
+        return lodash.delay(_refresh.bind(this, _, refresh), 2000);
       }
     }
   });
