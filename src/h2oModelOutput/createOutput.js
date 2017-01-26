51dadb9f601f7738a74a53453c1cdb0f1d9c6219
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-26 14:40:09 -0800

    refactor glm scoring history plot

--- src/h2oModelOutput/createOutput.js
+++ src/h2oModelOutput/createOutput.js
@@ -129,7 +129,34 @@ export default function createOutput(_) {
       if (table) {
         lambdaSearchParameter = lodash.find(_.model.parameters, parameter => parameter.name === 'lambda_search');
         if (lambdaSearchParameter != null ? lambdaSearchParameter.actual_value : void 0) {
-          renderPlot(_, 'Scoring History', false, _.plot(g => g(g.path(g.position('lambda', 'explained_deviance_train'), g.strokeColor(g.value('#1f77b4'))), g.path(g.position('lambda', 'explained_deviance_test'), g.strokeColor(g.value('#ff7f0e'))), g.point(g.position('lambda', 'explained_deviance_train'), g.strokeColor(g.value('#1f77b4'))), g.point(g.position('lambda', 'explained_deviance_test'), g.strokeColor(g.value('#ff7f0e'))), g.from(table))));
+          const gFunction = g => {
+            return g(
+              g.path(
+                g.position('lambda', 'explained_deviance_train'),
+                g.strokeColor(g.value('#1f77b4'))
+              ),
+              g.path(
+                g.position('lambda', 'explained_deviance_test'),
+                g.strokeColor(g.value('#ff7f0e'))
+              ),
+              g.point(
+                g.position('lambda', 'explained_deviance_train'),
+                g.strokeColor(g.value('#1f77b4'))
+              ),
+              g.point(
+                g.position('lambda', 'explained_deviance_test'),
+                g.strokeColor(g.value('#ff7f0e'))
+              ),
+              g.from(table)
+            )
+          };
+          const plotFunction = _.plot(gFunction);
+          renderPlot(
+            _,
+            'Scoring History',
+            false,
+            plotFunction
+          );
         } else {
           renderPlot(_, 'Scoring History', false, _.plot(g => g(g.path(g.position('iteration', 'objective'), g.strokeColor(g.value('#1f77b4'))), g.point(g.position('iteration', 'objective'), g.strokeColor(g.value('#1f77b4'))), g.from(table))));
         }
