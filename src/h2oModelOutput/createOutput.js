641cf1aeab07826c479e2ee8a6556466528253d2
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-27 22:39:39 -0800

    turn on rule no-unsused-vars for createOuput

--- src/h2oModelOutput/createOutput.js
+++ src/h2oModelOutput/createOutput.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: "error"*/
+
 import getAucAsLabel from './getAucAsLabel';
 import getThresholdsAndCriteria from './getThresholdsAndCriteria';
 import renderPlot from './renderPlot';
