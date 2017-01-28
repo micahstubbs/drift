11532265ba0cc14394b922c43705c77a90b0c3c9
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-27 17:26:12 -0800

    abstract out renderStackedEnsemblePlots

--- build/flow.js
+++ build/flow.js
@@ -5055,6 +5055,38 @@
       }
     }
 
+    function renderStackedEnsemblePlots(_, table) {
+      let plotFunction;
+      table = _.inspect('output - training_metrics - Metrics for Thresholds', _.model);
+      if (typeof table !== 'undefined') {
+        plotFunction = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
+        // TODO Mega-hack alert.
+        // Last arg thresholdsAndCriteria applicable only to
+        // ROC charts for binomial models.
+        renderPlot(_, `ROC Curve - Training Metrics${ getAucAsLabel(_, _.model, 'output - training_metrics') }`, false, plotFunction, getThresholdsAndCriteria(_, table, 'output - training_metrics - Maximum Metrics'));
+      }
+      table = _.inspect('output - validation_metrics - Metrics for Thresholds', _.model);
+      if (typeof table !== 'undefined') {
+        plotFunction = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
+        // TODO Mega-hack alert.
+        // Last arg thresholdsAndCriteria applicable only to
+        // ROC charts for binomial models.
+        renderPlot(_, `'ROC Curve - Validation Metrics${ getAucAsLabel(_, _.model, 'output - validation_metrics') }`, false, plotFunction, getThresholdsAndCriteria(_, table, 'output - validation_metrics - Maximum Metrics'));
+      }
+      table = _.inspect('output - cross_validation_metrics - Metrics for Thresholds', _.model);
+      if (typeof table !== 'undefined') {
+        plotFunction = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
+        // TODO Mega-hack alert.
+        // Last arg thresholdsAndCriteria applicable only to
+        // ROC charts for binomial models.
+        renderPlot(_, `ROC Curve - Cross Validation Metrics${ getAucAsLabel(_, _.model, 'output - cross_validation_metrics') }`, false, plotFunction, getThresholdsAndCriteria(_, table, 'output - cross_validation_metrics - Maximum Metrics'));
+      }
+      table = _.inspect('output - Variable Importances', _.model);
+      if (typeof table !== 'undefined') {
+        renderPlot(_, 'Variable Importances', false, _.plot(g => g(g.rect(g.position('scaled_importance', 'variable')), g.from(table), g.limit(25))));
+      }
+    }
+
     const flowPrelude$33 = flowPreludeFunction();
 
     function createOutput(_) {
@@ -5172,37 +5204,7 @@
           renderConfusionMatrices(_);
           break;
         case 'stackedensemble':
-          table = _.inspect('output - training_metrics - Metrics for Thresholds', _.model);
-          if (typeof table !== 'undefined') {
-            plotter = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
-
-            // TODO Mega-hack alert.
-            // Last arg thresholdsAndCriteria applicable only to
-            // ROC charts for binomial models.
-            renderPlot(_, `ROC Curve - Training Metrics${ getAucAsLabel(_, _.model, 'output - training_metrics') }`, false, plotter, getThresholdsAndCriteria(_, table, 'output - training_metrics - Maximum Metrics'));
-          }
-          table = _.inspect('output - validation_metrics - Metrics for Thresholds', _.model);
-          if (typeof table !== 'undefined') {
-            plotter = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
-
-            // TODO Mega-hack alert.
-            // Last arg thresholdsAndCriteria applicable only to
-            // ROC charts for binomial models.
-            renderPlot(_, `'ROC Curve - Validation Metrics${ getAucAsLabel(_, _.model, 'output - validation_metrics') }`, false, plotter, getThresholdsAndCriteria(_, table, 'output - validation_metrics - Maximum Metrics'));
-          }
-          table = _.inspect('output - cross_validation_metrics - Metrics for Thresholds', _.model);
-          if (typeof table !== 'undefined') {
-            plotter = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
-
-            // TODO Mega-hack alert.
-            // Last arg thresholdsAndCriteria applicable only to
-            // ROC charts for binomial models.
-            renderPlot(_, `ROC Curve - Cross Validation Metrics${ getAucAsLabel(_, _.model, 'output - cross_validation_metrics') }`, false, plotter, getThresholdsAndCriteria(_, table, 'output - cross_validation_metrics - Maximum Metrics'));
-          }
-          table = _.inspect('output - Variable Importances', _.model);
-          if (typeof table !== 'undefined') {
-            renderPlot(_, 'Variable Importances', false, _.plot(g => g(g.rect(g.position('scaled_importance', 'variable')), g.from(table), g.limit(25))));
-          }
+          renderStackedEnsemblePlots(_, table);
           renderConfusionMatrices(_);
           break;
         default:
--- src/h2oModelOutput/createOutput.js
+++ src/h2oModelOutput/createOutput.js
@@ -16,6 +16,7 @@ import plotKMeansScoringHistory from './plotKMeansScoringHistory';
 import renderGLMPlots from './renderGLMPlots';
 import renderDeepNetPlots from './renderDeepNetPlots';
 import renderTreeAlgoPlots from './renderTreeAlgoPlots';
+import renderStackedEnsemblePlots from './renderStackedEnsemblePlots';
 
 import { flowPreludeFunction } from '../flowPreludeFunction';
 const flowPrelude = flowPreludeFunction();
@@ -135,37 +136,7 @@ export default function createOutput(_) {
       renderConfusionMatrices(_);
       break;
     case 'stackedensemble':
-      table = _.inspect('output - training_metrics - Metrics for Thresholds', _.model);
-      if (typeof table !== 'undefined') {
-        plotter = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
-
-          // TODO Mega-hack alert.
-          // Last arg thresholdsAndCriteria applicable only to
-          // ROC charts for binomial models.
-        renderPlot(_, `ROC Curve - Training Metrics${getAucAsLabel(_, _.model, 'output - training_metrics')}`, false, plotter, getThresholdsAndCriteria(_, table, 'output - training_metrics - Maximum Metrics'));
-      }
-      table = _.inspect('output - validation_metrics - Metrics for Thresholds', _.model);
-      if (typeof table !== 'undefined') {
-        plotter = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
-
-          // TODO Mega-hack alert.
-          // Last arg thresholdsAndCriteria applicable only to
-          // ROC charts for binomial models.
-        renderPlot(_, `'ROC Curve - Validation Metrics${getAucAsLabel(_, _.model, 'output - validation_metrics')}`, false, plotter, getThresholdsAndCriteria(_, table, 'output - validation_metrics - Maximum Metrics'));
-      }
-      table = _.inspect('output - cross_validation_metrics - Metrics for Thresholds', _.model);
-      if (typeof table !== 'undefined') {
-        plotter = _.plot(g => g(g.path(g.position('fpr', 'tpr')), g.line(g.position(g.value(1), g.value(0)), g.strokeColor(g.value('red'))), g.from(table), g.domainX_HACK(0, 1), g.domainY_HACK(0, 1)));
-
-          // TODO Mega-hack alert.
-          // Last arg thresholdsAndCriteria applicable only to
-          // ROC charts for binomial models.
-        renderPlot(_, `ROC Curve - Cross Validation Metrics${getAucAsLabel(_, _.model, 'output - cross_validation_metrics')}`, false, plotter, getThresholdsAndCriteria(_, table, 'output - cross_validation_metrics - Maximum Metrics'));
-      }
-      table = _.inspect('output - Variable Importances', _.model);
-      if (typeof table !== 'undefined') {
-        renderPlot(_, 'Variable Importances', false, _.plot(g => g(g.rect(g.position('scaled_importance', 'variable')), g.from(table), g.limit(25))));
-      }
+      renderStackedEnsemblePlots(_, table);
       renderConfusionMatrices(_);
       break;
     default:
