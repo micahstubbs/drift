e5f440cd848ac9ee822e3a8ca91e14eb5c479883
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-30 16:05:30 -0800

    refactor renderFrame

--- build/flow.js
+++ build/flow.js
@@ -827,9 +827,12 @@
     }
 
     function renderFrame(_, _chunkSummary, _distributionSummary, frame) {
-      renderGrid(_, _.plot(g => g(g.select(), g.from(_.inspect('columns', frame)))));
-      renderPlot(_chunkSummary, _.plot(g => g(g.select(), g.from(_.inspect('Chunk compression summary', frame)))));
-      return renderPlot(_distributionSummary, _.plot(g => g(g.select(), g.from(_.inspect('Frame distribution summary', frame)))));
+      const gridPlotFunction = _.plot(g => g(g.select(), g.from(_.inspect('columns', frame))));
+      const chunkSummaryPlotFunction = _.plot(g => g(g.select(), g.from(_.inspect('Chunk compression summary', frame))));
+      const distributionSummaryPlotFunction = _.plot(g => g(g.select(), g.from(_.inspect('Frame distribution summary', frame))));
+      renderGrid(_, gridPlotFunction);
+      renderPlot(_chunkSummary, chunkSummaryPlotFunction);
+      return renderPlot(_distributionSummary, distributionSummaryPlotFunction);
     }
 
     function formatBytes(bytes) {
