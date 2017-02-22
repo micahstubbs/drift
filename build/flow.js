fe0bda4f79e27293b1d7c98abb57632bf92ff73f
Micah Stubbs <micah.stubbs@gmail.com>
2017-02-22 12:50:01 -0800

    error now renders in flow ui. still missing error message.

--- build/flow.js
+++ build/flow.js
@@ -12270,12 +12270,12 @@
         return ft((error, result) => {
           console.log('error from flowCoffeescript render evaluate', error);
           console.log('result from flowCoffeescript render evaluate', result);
-          const _ref = result._flow_;
           if (error) {
+            console.log('output from flowCoffeescript evaluate', output);
             output.error(new Flow.Error('Error evaluating cell', error));
             return output.end();
           }
-          if (result != null ? _ref != null ? _ref.render : void 0 : void 0) {
+          if (result != null ? result._flow_ != null ? result._flow_.render : void 0 : void 0) {
             return output.data(result._flow_.render(() => output.end()));
           }
           return output.data(Flow.objectBrowser(_, (() => output.end())('output', result)));
